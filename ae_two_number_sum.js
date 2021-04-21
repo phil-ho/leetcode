18059196d1eb76ddb9b491efc3d9940daf8bc0c9
Phillip Ho <philasan@gmail.com>
2021-04-20 23:46:24 -0700

    an O(n) O(1) solution to TwoSum

--- ae_two_number_sum.js
+++ ae_two_number_sum.js
@@ -15,3 +15,38 @@ function twoNumberSum(array, targetSum) {
 	return [];
 }
 // Time: O(n), Space: O(n)
+
+function twoNumberSum2(array, targetSum) {
+  // sort array for order
+	const sortedArray = array.slice().sort((a,b) => a - b);
+	console.log(sortedArray);
+	// use 2 pointers from outside
+	let left = 0;
+	let right = sortedArray.length - 1;
+
+	// iterate through array
+		// if sum is equal to target, return
+		// if sum is larger than target move the right pointer in
+		// if sum is smaller than target, move the left pointer in
+		// if left pointer === right pointer, return empty array because no solution
+	while(left !== right) {
+		const num1 = sortedArray[left];
+		const num2 = sortedArray[right];
+		const sum = num1 + num2;
+		console.log(targetSum, sum, left, right);
+
+		if (targetSum === sum) {
+			return [num1, num2];
+		}
+
+		if (targetSum < sum) {
+			right--;
+		} else {
+			left++;
+		}
+	}
+
+	return [];
+}
+// Time: O(n)
+// Space: O(1)
