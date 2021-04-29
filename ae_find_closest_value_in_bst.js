5da5be8a9e5d1c3d131956384845797a0dbf9b27
Phillip Ho <philasan@gmail.com>
2021-04-29 00:16:49 -0700

    1st solution

--- ae_find_closest_value_in_bst.js
+++ ae_find_closest_value_in_bst.js
@@ -1,12 +1,47 @@
 function findClosestValueInBst(tree, target) {
-  // Write your code here.
+  // i: BST tree where values are integers
+	// O: closest value (int)
+	// c: none
+	// e: none
+	// only 1 closest val
+
+	// what is closest value?
+		// when value is target or
+	  // when value's difference is smaller than others
+	let closestValue = tree.value;
+
+
+	let traverse = (tree) => {
+		// if value is target return value
+		if (tree.value === target) {
+			closestValue = tree.value;
+			return;
+		}
+
+		// is the difference between the value of this tree and the target
+		// the smallest we've seen?
+			// save value as closest value
+		const diff = Math.abs(target - tree.value);
+
+		if (diff < Math.abs(target - closestValue)) {
+			closestValue = tree.value;
+		}
+
+		// if target less than value
+			// go left if left exists
+		// if target is greater than value
+			// go right if right exists
+		if (target < tree.value && tree.left) {
+		  traverse(tree.left);
+		}
+		if (target > tree.value && tree.right) {
+			traverse(tree.right);
+		}
+	}
+
+	traverse(tree);
+
+	return closestValue
 }
 
-// This is the class of the input tree. Do not edit.
-class BST {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
+// O(n) time, O(n)? space
