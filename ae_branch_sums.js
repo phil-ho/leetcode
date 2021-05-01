f89789ea9d02be2748c0ae28e58b93dc8689236a
Phillip Ho <philasan@gmail.com>
2021-05-01 00:29:31 -0700

    soln 2 w helper, O(n)

--- ae_branch_sums.js
+++ ae_branch_sums.js
@@ -1,35 +1,22 @@
-// input a root tree node
-// output an array with the branch's values summed up
-// no constraints
-// root can be null
 function branchSums(root) {
-  // inspect root
-	// if root is null, return []
-
-	// create array
-	// traverse children
-		// concat result from recursive call to branchSums(child) to array
-
-	// if array has length
-		// add this root's value to the array values
-	// else
-		// push this roots value into
-
-	//return array
-
-	if (root === null) {
-		return [];
-	}
-
-	let sums = [];
-	sums = sums.concat(branchSums(root.left));
-	sums = sums.concat(branchSums(root.right));
-
-	if (sums.length > 0) {
-		sums = sums.map((num) => num + root.value);
-	} else {
-		sums.push(root.value);
-	}
-
+  const sums = [];
+	branchSumsHelper(root, 0, sums);
 	return sums;
 }
+
+function branchSumsHelper(root, total, sums) {
+	if (root === null) {
+		return;
+	}
+
+	const newTotal = total + root.value;
+
+	if (root.left === null && root.right === null) {
+		sums.push(newTotal);
+		return;
+	}
+
+	branchSumsHelper(root.left, newTotal, sums);
+	branchSumsHelper(root.right, newTotal, sums);
+}
+// O(n) Time, O(n) space
