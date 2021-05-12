8556d06aab66d282f12ab2ca941603531488f6e3
Phillip Ho <philasan@gmail.com>
2021-05-11 22:45:39 -0700

    wo recursion

--- ae/remove_duplicates_from_linked_list.js
+++ ae/remove_duplicates_from_linked_list.js
@@ -1,29 +1,16 @@
-function removeDuplicatesFromLinkedListHelper(thisNode, nextNode) {
-  if (thisNode === null) {
-    return;
-  }
-
-  if (nextNode === null) {
-    thisNode.next = null;
-    return;
-  }
-
-  if (thisNode.value === nextNode.value) {
-    removeDuplicatesFromLinkedListHelper(thisNode, nextNode.next);
-  } else {
-    thisNode.next = nextNode;
-    removeDuplicatesFromLinkedListHelper(nextNode, nextNode?.next || null);
-  }
-}
-
 function removeDuplicatesFromLinkedList(linkedList) {
-  // traverse a sorted linked list, removing duplicate values from list
+  let currentNode = linkedList;
 
-	// i: linked list, sorted with duplicates
-	// o: linked list, sorted with no duplicates
-	// c: none
-	// e: head is null
-	removeDuplicatesFromLinkedListHelper(linkedList, linkedList.next);
+	while(currentNode !== null) {
+		let nextDistinctNode = currentNode.next;
+
+		while(nextDistinctNode?.value === currentNode.value) {
+			nextDistinctNode = nextDistinctNode.next;
+		}
+
+		currentNode.next = nextDistinctNode;
+		currentNode = nextDistinctNode;
+	}
 
   return linkedList;
 }
