6e9d1b331fa29d4b051ae9dfd18095f501500fea
Phillip Ho <philasan@gmail.com>
2021-10-24 22:45:30 -0700

    selectionSort 2, recursive

--- ae/ae_selection_sort.js
+++ ae/ae_selection_sort.js
@@ -4,20 +4,21 @@ function swap(array, index1, index2) {
   array[index2] = temp;
 }
 
-function selectionSort(array) {
-  for (let i = 0; i < array.length; i++) {
-    let indexOfSmallest = i;
-
-    for (let j = i + 1; j < array.length; j++) {
-      if (array[j] < array[indexOfSmallest]) {
-        indexOfSmallest = j;
-      }
-    }
-
-    swap(array, i, indexOfSmallest);
+function selectionSort(array, start = 0) {
+  if (start > array.length - 1) {
+    return array;
   }
 
-  return array;
+  let indexOfSmallestValue = start;
+  for (let i = start + 1; i < array.length; i++) {
+    if (array[i] < array[indexOfSmallestValue]) {
+      indexOfSmallestValue = i;
+    }
+  }
+
+  swap(array, start, indexOfSmallestValue);
+
+  return selectionSort(array, start + 1);
 }
 
 module.exports = selectionSort;
